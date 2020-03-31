7fc110e98ea5c8f4d007a3dfabf16a4a4f7bb712
RuWolf <rudolf.bexshtedt@mail.ru>
2020-04-01 00:02:51 +0300

    stert

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ShahvatsDashboard from "./components/ShahvatsDashboard";
+
+function App() {
+  return (
+    <ShahvatsDashboard/>
+  );
+}
+
+export default App;
---
+++ client/src/components/ShahvatsDashboard.js
@@ -0,0 +1,94 @@
+import React, {Component} from 'react';
+import '../styles/dashboard.css'
+
+class ShahvatsDashboard extends Component {
+  render() {
+    return (
+      <div className="shahvats-dashboard">
+        <div className='row'>
+          <div id='8a' className='white-field'></div>
+          <div id='8b' className='black-field'></div>
+          <div id='8c' className='white-field'></div>
+          <div id='8d' className='black-field'></div>
+          <div id='8e' className='white-field'></div>
+          <div id='8f' className='black-field'></div>
+          <div id='8g' className='white-field'></div>
+          <div id='8h' className='black-field'></div>
+        </div>
+        <div className='row'>
+          <div id='7a' className='black-field'></div>
+          <div id='7b' className='white-field'></div>
+          <div id='7c' className='black-field'></div>
+          <div id='7d' className='white-field'></div>
+          <div id='7e' className='black-field'></div>
+          <div id='7f' className='white-field'></div>
+          <div id='7g' className='black-field'></div>
+          <div id='7h' className='white-field'></div>
+        </div>
+        <div className='row'>
+          <div id='6a' className='white-field'></div>
+          <div id='6b' className='black-field'></div>
+          <div id='6c' className='white-field'></div>
+          <div id='6d' className='black-field'></div>
+          <div id='6e' className='white-field'></div>
+          <div id='6f' className='black-field'></div>
+          <div id='6g' className='white-field'></div>
+          <div id='6h' className='black-field'></div>
+        </div>
+        <div className='row'>
+          <div id='5a' className='black-field'></div>
+          <div id='5b' className='white-field'></div>
+          <div id='5c' className='black-field'></div>
+          <div id='5d' className='white-field'></div>
+          <div id='5e' className='black-field'></div>
+          <div id='5f' className='white-field'></div>
+          <div id='5g' className='black-field'></div>
+          <div id='5h' className='white-field'></div>
+        </div>
+        <div className='row'>
+          <div id='4a' className='white-field'></div>
+          <div id='4b' className='black-field'></div>
+          <div id='4c' className='white-field'></div>
+          <div id='4d' className='black-field'></div>
+          <div id='4e' className='white-field'></div>
+          <div id='4f' className='black-field'></div>
+          <div id='4g' className='white-field'></div>
+          <div id='4h' className='black-field'></div>
+        </div>
+        <div className='row'>
+          <div id='3a' className='black-field'></div>
+          <div id='3b' className='white-field'></div>
+          <div id='3c' className='black-field'></div>
+          <div id='3d' className='white-field'></div>
+          <div id='3e' className='black-field'></div>
+          <div id='3f' className='white-field'></div>
+          <div id='3g' className='black-field'></div>
+          <div id='3h' className='white-field'></div>
+        </div>
+        <div className='row'>
+          <div id='2a' className='white-field'></div>
+          <div id='2b' className='black-field'></div>
+          <div id='2c' className='white-field'></div>
+          <div id='2d' className='black-field'></div>
+          <div id='2e' className='white-field'></div>
+          <div id='2f' className='black-field'></div>
+          <div id='2g' className='white-field'></div>
+          <div id='2h' className='black-field'></div>
+        </div>
+        <div className='row'>
+          <div id='1a' className='black-field'></div>
+          <div id='1b' className='white-field'></div>
+          <div id='1c' className='black-field'></div>
+          <div id='1d' className='white-field'></div>
+          <div id='1e' className='black-field'></div>
+          <div id='1f' className='white-field'></div>
+          <div id='1g' className='black-field'></div>
+          <div id='1h' className='white-field'></div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default ShahvatsDashboard;
