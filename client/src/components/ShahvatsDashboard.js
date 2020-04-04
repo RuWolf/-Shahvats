c35c2db6a6173253a4f545f284a61fe1b131c4e1
RuWolf <rudolf.bexshtedt@mail.ru>
2020-04-04 19:00:38 +0300

    add black & render dashboard

--- client/src/components/ShahvatsDashboard.js
+++ client/src/components/ShahvatsDashboard.js
@@ -8,109 +8,118 @@ const whiteHorse = require('../images/white-horse.png');
 const whiteElephant = require('../images/white-elephant.png');
 const whitePawn = require('../images/white-pawn.png');
 
+const blackKing = require('../images/black-king.png');
+const blackQueen = require('../images/black-queen.png');
+const blackOfficer = require('../images/black-officer.png');
+const blackHorse = require('../images/black-horse.png');
+const blackElephant = require('../images/black-elephant.png');
+const blackPawn = require('../images/black-pawn.png');
+
 class ShahvatsDashboard extends Component {
-
-
 
   clickField = (event) => {
     event.target.classList.toggle('active-square')
     console.log(event.target);
   };
+
+  showField = (element) => {
+    let result = [];
+    for (let i = 0; i < 64; i = i + 8) {
+      let row = [];
+      for (let j = i; j < i + 8; j++) {
+        if (element[j].value) {
+          row.push(
+            <div key={element[j].address} id={element[j].address} className={element[j].color}
+                 onClick={this.clickField}>
+              <img src={element[j].value}/>
+            </div>)
+        } else {
+          row.push(
+            <div key={element[j].address} id={element[j].address} className={element[j].color}
+                 onClick={this.clickField}>
+            </div>)
+        }
+      }
+      result.push(
+        <div key={i} className='row'>
+          {row}
+        </div>
+      )
+    }
+    return result;
+  };
+
+
   render() {
+    let position = [
+      {address: 'a8', value: blackElephant, color: 'white-field'},
+      {address: 'b1', value: blackHorse, color: 'black-field'},
+      {address: 'c8', value: blackOfficer, color: 'white-field'},
+      {address: 'd8', value: blackQueen, color: 'black-field'},
+      {address: 'e8', value: blackKing, color: 'white-field'},
+      {address: 'f8', value: blackOfficer, color: 'black-field'},
+      {address: 'g8', value: blackHorse, color: 'white-field'},
+      {address: 'h8', value: blackElephant, color: 'black-field'},
+      {address: 'a7', value: blackPawn, color: 'black-field'},
+      {address: 'b7', value: blackPawn, color: 'white-field'},
+      {address: 'c7', value: blackPawn, color: 'black-field'},
+      {address: 'd7', value: blackPawn, color: 'white-field'},
+      {address: 'e7', value: blackPawn, color: 'black-field'},
+      {address: 'f7', value: blackPawn, color: 'white-field'},
+      {address: 'g7', value: blackPawn, color: 'black-field'},
+      {address: 'h7', value: blackPawn, color: 'white-field'},
+      {address: 'a6', value: '', color: 'white-field'},
+      {address: 'b6', value: '', color: 'black-field'},
+      {address: 'c6', value: '', color: 'white-field'},
+      {address: 'd6', value: '', color: 'black-field'},
+      {address: 'e6', value: '', color: 'white-field'},
+      {address: 'f6', value: '', color: 'black-field'},
+      {address: 'g6', value: '', color: 'white-field'},
+      {address: 'h6', value: '', color: 'black-field'},
+      {address: 'a5', value: '', color: 'black-field'},
+      {address: 'b5', value: '', color: 'white-field'},
+      {address: 'c5', value: '', color: 'black-field'},
+      {address: 'd5', value: '', color: 'white-field'},
+      {address: 'e5', value: '', color: 'black-field'},
+      {address: 'f5', value: '', color: 'white-field'},
+      {address: 'g5', value: '', color: 'black-field'},
+      {address: 'h5', value: '', color: 'white-field'},
+      {address: 'a4', value: '', color: 'white-field'},
+      {address: 'b4', value: '', color: 'black-field'},
+      {address: 'c4', value: '', color: 'white-field'},
+      {address: 'd4', value: '', color: 'black-field'},
+      {address: 'e4', value: '', color: 'white-field'},
+      {address: 'f4', value: '', color: 'black-field'},
+      {address: 'g4', value: '', color: 'white-field'},
+      {address: 'h4', value: '', color: 'black-field'},
+      {address: 'a3', value: '', color: 'black-field'},
+      {address: 'b3', value: '', color: 'white-field'},
+      {address: 'c3', value: '', color: 'black-field'},
+      {address: 'd3', value: '', color: 'white-field'},
+      {address: 'e3', value: '', color: 'black-field'},
+      {address: 'f3', value: '', color: 'white-field'},
+      {address: 'g3', value: '', color: 'black-field'},
+      {address: 'h3', value: '', color: 'white-field'},
+      {address: 'a2', value: whitePawn, color: 'white-field'},
+      {address: 'b2', value: whitePawn, color: 'black-field'},
+      {address: 'c2', value: whitePawn, color: 'white-field'},
+      {address: 'd2', value: whitePawn, color: 'black-field'},
+      {address: 'e2', value: whitePawn, color: 'white-field'},
+      {address: 'f2', value: whitePawn, color: 'black-field'},
+      {address: 'g2', value: whitePawn, color: 'white-field'},
+      {address: 'h2', value: whitePawn, color: 'black-field'},
+      {address: 'a1', value: whiteElephant, color: 'black-field'},
+      {address: 'b1', value: whiteHorse, color: 'white-field'},
+      {address: 'c1', value: whiteOfficer, color: 'black-field'},
+      {address: 'd1', value: whiteQueen, color: 'white-field'},
+      {address: 'e1', value: whiteKing, color: 'black-field'},
+      {address: 'f1', value: whiteOfficer, color: 'white-field'},
+      {address: 'g1', value: whiteHorse, color: 'black-field'},
+      {address: 'h1', value: whiteElephant, color: 'white-field'},
+    ];
     return (
       <div className="shahvats-dashboard">
-        <div className='row'>
-          <div id='8a' className='white-field' onClick={this.clickField}>
-            <img src={whiteKing}/>
-          </div>
-          <div id='8b' className='black-field' onClick={this.clickField}>
-            <img src={whiteQueen}/>
-          </div>
-          <div id='8c' className='white-field' onClick={this.clickField}>
-            <img src={whiteOfficer}/>
-          </div>
-          <div id='8d' className='black-field' onClick={this.clickField}>
-            <img src={whiteHorse}/>
-          </div>
-          <div id='8e' className='white-field' onClick={this.clickField}>
-            <img src={whiteElephant}/>
-          </div>
-          <div id='8f' className='black-field' onClick={this.clickField}>
-            <img src={whitePawn}/>
-          </div>
-          <div id='8g' className='white-field' onClick={this.clickField}></div>
-          <div id='8h' className='black-field' onClick={this.clickField}></div>
-        </div>
-        <div className='row'>
-          <div id='7a' className='black-field'></div>
-          <div id='7b' className='white-field'></div>
-          <div id='7c' className='black-field'></div>
-          <div id='7d' className='white-field'></div>
-          <div id='7e' className='black-field'></div>
-          <div id='7f' className='white-field'></div>
-          <div id='7g' className='black-field'></div>
-          <div id='7h' className='white-field'></div>
-        </div>
-        <div className='row'>
-          <div id='6a' className='white-field'></div>
-          <div id='6b' className='black-field'></div>
-          <div id='6c' className='white-field'></div>
-          <div id='6d' className='black-field'></div>
-          <div id='6e' className='white-field'></div>
-          <div id='6f' className='black-field'></div>
-          <div id='6g' className='white-field'></div>
-          <div id='6h' className='black-field'></div>
-        </div>
-        <div className='row'>
-          <div id='5a' className='black-field'></div>
-          <div id='5b' className='white-field'></div>
-          <div id='5c' className='black-field'></div>
-          <div id='5d' className='white-field'></div>
-          <div id='5e' className='black-field'></div>
-          <div id='5f' className='white-field'></div>
-          <div id='5g' className='black-field'></div>
-          <div id='5h' className='white-field'></div>
-        </div>
-        <div className='row'>
-          <div id='4a' className='white-field'></div>
-          <div id='4b' className='black-field'></div>
-          <div id='4c' className='white-field'></div>
-          <div id='4d' className='black-field'></div>
-          <div id='4e' className='white-field'></div>
-          <div id='4f' className='black-field'></div>
-          <div id='4g' className='white-field'></div>
-          <div id='4h' className='black-field'></div>
-        </div>
-        <div className='row'>
-          <div id='3a' className='black-field'></div>
-          <div id='3b' className='white-field'></div>
-          <div id='3c' className='black-field'></div>
-          <div id='3d' className='white-field'></div>
-          <div id='3e' className='black-field'></div>
-          <div id='3f' className='white-field'></div>
-          <div id='3g' className='black-field'></div>
-          <div id='3h' className='white-field'></div>
-        </div>
-        <div className='row'>
-          <div id='2a' className='white-field'></div>
-          <div id='2b' className='black-field'></div>
-          <div id='2c' className='white-field'></div>
-          <div id='2d' className='black-field'></div>
-          <div id='2e' className='white-field'></div>
-          <div id='2f' className='black-field'></div>
-          <div id='2g' className='white-field'></div>
-          <div id='2h' className='black-field'></div>
-        </div>
-        <div className='row'>
-          <div id='1a' className='black-field'></div>
-          <div id='1b' className='white-field'></div>
-          <div id='1c' className='black-field'></div>
-          <div id='1d' className='white-field'></div>
-          <div id='1e' className='black-field'></div>
-          <div id='1f' className='white-field'></div>
-          <div id='1g' className='black-field'></div>
-          <div id='1h' className='white-field'></div>
-        </div>
+        {this.showField(position)}
       </div>
     );
   }
